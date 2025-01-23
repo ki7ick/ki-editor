2e7dfcf809cec3c778da5e6914415ebd085e95a5
kickr <ki7ick@gmail.com>
2025-01-23 11:39:49 +0800

    chore: support jest

---
+++ packages/core/jest.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom', // 如果你需要浏览器环境
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    "\\.module\\.css$": "identity-obj-proxy",
+    "\\.css$": "identity-obj-proxy"
+  }
+};
